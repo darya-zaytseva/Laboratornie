2cbd1aa931ff14075d2b298c481c376aaef75984
darya-zaytseva <darya.zaytseva.2007@bk.ru>
2025-01-29 13:11:58 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,12 +4,16 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите x: ");
         double x = in.nextDouble();
-        double sum = 0;
-        for (double n = 0; n<=x; n++){
-            double schet = (Math.pow(x,(2*n+1))/(2*n+1));
-            sum+=schet;
+        if (Math.abs(x)>1)
+            System.out.println("Число меньше 1");
+        else {
+            double sum = 0;
+            for (double n = 0; n <= x; n++) {
+                double schet = (Math.pow(x, (2 * n + 1)) / (2 * n + 1));
+                sum += schet;
+            }
+            double b = sum * 2;
+            System.out.println(b);
         }
-        double b = sum*2;
-        System.out.println(b);
     }
 }
