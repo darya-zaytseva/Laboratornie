e18f934bbdb2eeaae4e63e9d43f34569cce71980
darya-zaytseva <darya.zaytseva.2007@bk.ru>
2025-02-12 11:04:03 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,39 +4,40 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите n: ");
         int n = in.nextInt();
-        int[][]sortArr = new int[n][n];
-        for(int i=0; i<sortArr.length; i++){
-            for(int j=0; j<sortArr.length; j++){
+        int[][] sortArr = new int[n][n];
+        for (int i = 0; i < sortArr.length; i++) {
+            for (int j = 0; j < sortArr.length; j++) {
                 System.out.println("Введите значение элемента:");
-                sortArr[i][j]=in.nextInt();
+                sortArr[i][j] = in.nextInt();
             }
         }
-        selectionSort(sortArr, n);
-        for(int i=0; i<sortArr.length; i++){
-            for(int j=0; j<sortArr.length; j++) {
+        combSort(sortArr, n);
+        for (int i = 0; i < sortArr.length; i++) {
+            for (int j = 0; j < sortArr.length; j++) {
                 System.out.println(sortArr[i][j] + " ");
             }
             System.out.println();
         }
     }
-    public static void selectionSort(int[][] sortArr, int n){
-        int[] dia = new int[n];
-        for(int i=0; i<sortArr.length; i++){
-            dia[i]=sortArr[i][n-1-i];
-            }
-        for(int i=0; i<dia.length-1; i++) {
-                int min=i;
-            for(int j=i+1; j<dia.length; j++) {
-                if(dia[j]<dia[min]){
-                    min=j;
+    public static void combSort(int[][] sortArr, int n){
+        double factor = 1.247;
+        int step = n;
+        while(step>=1){
+            for(int i = 0; i+step<n; i++){
+                int peri1 = i;
+                int perj1 = n-1-i;
+                int peri2 = i+step;
+                int perj2 = n-1-i-step;
+                if(sortArr[peri1][perj1]>sortArr[peri2][perj2]){
+                    swap(sortArr,peri1, perj1, peri2, perj2);
                 }
             }
-            int per = dia[i];
-            dia[i]=dia[min];
-            dia[min]=per;
+            step/=factor;
         }
-        for(int i=0; i<sortArr.length; i++){
-            sortArr[i][n-1-i]=dia[i];
-        }
+    }
+    static void swap(int[][]sortArr, int peri1, int perj1, int peri2, int perj2){
+        int swap = sortArr[peri1][perj1];
+        sortArr[peri1][perj1]=sortArr[peri2][perj2];
+        sortArr[peri2][perj2]=swap;
     }
 }
