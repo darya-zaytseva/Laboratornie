dd820a6c24aa0be26b483cb9a4afadab0fe3c3b6
darya-zaytseva <darya.zaytseva.2007@bk.ru>
2025-03-02 14:37:08 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,58 @@
 import java.util.Scanner;
 public class Main {
+    public static class Schet{
+        private double x;
+        private double a;
+        private double b;
+        public Schet(double x, double a, double b){
+            this.x=x;
+            this.a=a;
+            this.b=b;
+        }
+        public double Schet1(){
+            return 3*this.x+5;
+        }
+        public double Schet2(){
+            if(this.a==this.b){
+                System.out.println("Деление на ноль(");
+                return 0;
+            }
+            return (this.a+this.b)/(this.a-this.b);
+        }
+        public double Schet3(){
+            if (this.x==0||this.a==0||this.b==0){
+                System.out.println("Деление на ноль(");
+                return 0;
+            }
+            double primer = this.a*this.x/this.b;
+            int prim = (int)Math.round(primer);
+            return factorial(prim);
+        }
+        private double factorial(int prim){
+            if (prim<0){
+                System.out.println("Отрицательные числа не вводить(");
+                return 0;
+            }
+            double f = 1;
+            for (int i=1; i<=prim; i++){
+                f*=i;
+            }
+            return f;
+        }
+    }
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите n: ");
-        int n = in.nextInt();
-        int[][] sortArr = new int[n][n];
-        for (int i = 0; i < sortArr.length; i++) {
-            for (int j = 0; j < sortArr.length; j++) {
-                System.out.println("Введите значение элемента:");
-                sortArr[i][j] = in.nextInt();
-            }
-        }
-        combSort(sortArr, n);
-        for (int i = 0; i < sortArr.length; i++) {
-            for (int j = 0; j < sortArr.length; j++) {
-                System.out.println(sortArr[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-    public static void combSort(int[][] sortArr, int n){
-        double factor = 1.247;
-        int step = n;
-        while(step>=1){
-            for(int i = 0; i+step<n; i++){
-                int peri1 = i;
-                int perj1 = n-1-i;
-                int peri2 = i+step;
-                int perj2 = n-1-i-step;
-                if(sortArr[peri1][perj1]>sortArr[peri2][perj2]){
-                    swap(sortArr,peri1, perj1, peri2, perj2);
-                }
-            }
-            step/=factor;
-        }
-    }
-    static void swap(int[][]sortArr, int peri1, int perj1, int peri2, int perj2){
-        int swap = sortArr[peri1][perj1];
-        sortArr[peri1][perj1]=sortArr[peri2][perj2];
-        sortArr[peri2][perj2]=swap;
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Введите значение x: ");
+        double x = scanner.nextDouble();
+        System.out.print("Введите значение a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Введите значение b: ");
+        double b = scanner.nextDouble();
+        Schet one = new Schet(x, a, b);
+        Schet two = new Schet(x, a, b);
+        Schet three = new Schet(x, a, b);
+        System.out.println("Результат для y=3x+5: " + one.Schet1());
+        System.out.println("Результат для y=(a+b)/(a-b): " + two.Schet2());
+        System.out.println("Результат для y=(ax/b)!: " + three.Schet3());
     }
 }
